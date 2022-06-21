7c49fbdb67e3d451025dbb38c95485f5501e64dc
HannahDalwai <hannahdalwai4@gmail.com>
2022-06-21 11:49:32 +0100

    fetching

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,42 @@
 <template>
-
+<div v-if="payload">
+  <div v-for="data in payload" :key="data">
+    <div v-for="payload in data" :key="payload">
+      <h2>
+        {{payload}}
+      </h2>
+    </div>
+ </div>
+</div>
+ <div v-else>
+  <h1>
+    Loading...
+  </h1>
+ </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import axios from 'axios';
 export default {
- 
   components: {
  
-  }
-}
+  },
+  data(){
+    return{
+     payload:null
+    }
+  },
+  mounted(){ 
+ 
+    axios.get('https://apitest.cimsoweb.com/api/innterchange/get_booking_units_request')
+    .then(res => {
+      this.payload = res.data
+      console.log(this.payload)
+    })
+    .catch(err => {
+      console.error(err); 
+    })
+
+}}
 </script>
