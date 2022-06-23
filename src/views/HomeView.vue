eb1c6a146b0ba16b47be8f7f5f48d8e8667e4b10
HannahDalwai <hannahdalwai4@gmail.com>
2022-06-23 11:09:10 +0100

    looping in a loop :)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,49 @@
 <template>
 <div v-if="payload">
-  <div v-for="data in payload" :key="data">
-    <div v-for="payload in data" :key="payload">
-      <h2>
-        {{payload}}
-      </h2>
-    </div>
- </div>
+    <div v-for="data in payload" :key="data">
+      <div v-for="payload in data" :key="payload">
+            <div v-for="payloads in payload" :key="payloads">
+              <ul class="list-group">
+                  <li class="list-group-item">{{payloads}}</li>
+              </ul>
+            </div>
+      </div>
+  </div>
+      <footer>
+        <div class="container">
+            <div class="row align-items-start">
+              <div class="col">
+                  <h4>bookings</h4>    
+              </div>
+              <div class="col">
+                  <h4>Services</h4>    
+              </div>
+              <div class="col">
+                  <h4>Contact</h4>    
+              </div>
+            </div>
+        </div>
+      </footer>
 </div>
  <div v-else>
   <h1>
     Loading...
   </h1>
- 
     <div class="loader">
-  <span class="loader-block"></span>
-  <span class="loader-block"></span>
-  <span class="loader-block"></span>
-  <span class="loader-block"></span>
-  <span class="loader-block"></span>
-  <span class="loader-block"></span>
-  <span class="loader-block"></span>
-  <span class="loader-block"></span>
-  <span class="loader-block"></span>
-</div>
-
+        <span class="loader-block"></span>
+        <span class="loader-block"></span>
+        <span class="loader-block"></span>
+        <span class="loader-block"></span>
+        <span class="loader-block"></span>
+        <span class="loader-block"></span>
+        <span class="loader-block"></span>
+        <span class="loader-block"></span>
+        <span class="loader-block"></span>
+    </div>
  </div>
-</template>
 
+</template>
 <script>
-// @ is an alias to /src
 import axios from 'axios';
 export default {
   components: {
@@ -40,9 +54,11 @@ export default {
      payload:null
     }
   },
+  
   mounted(){ 
  
     axios.get('https://apitest.cimsoweb.com/api/innterchange/get_booking_units_request')
+    
     .then(res => {
       this.payload = res.data
       console.log(this.payload)
@@ -50,11 +66,27 @@ export default {
     .catch(err => {
       console.error(err); 
     })
-
-}}
+}
+}
 </script>
 <style scoped>
+  
+  /*------------- the rest------------ */
+  ul{
+    width: 50%;
+    padding-bottom:20px;
+    margin-left: 20px;
+  }
+  /* li{
+  } */
 
+/*------------- Footer------------ */
+footer{
+  background-color: #2d2764;
+  color: rgba(255, 255, 255, 0.923);
+  height:200px;
+}
+/*------------- loader------------ */
 .loader {
   position: absolute;
   top: 50%;
@@ -64,7 +96,6 @@ export default {
   margin-left: -3.66667em;
   margin-top: -3.66667em;
 }
-
 .loader-block {
   position: absolute;
   top: 0;
@@ -77,7 +108,6 @@ export default {
   -webkit-animation: show 0.88s step-end infinite alternate, pulse 0.88s linear infinite alternate;
   animation: show 0.88s step-end infinite alternate, pulse 0.88s linear infinite alternate;
 }
-
 .loader-block:nth-child(1) {
   -moz-transform: translate(0, 0);
   -ms-transform: translate(0, 0);
@@ -86,7 +116,6 @@ export default {
   -webkit-animation-delay: 0.065s;
   animation-delay: 0.065s;
 }
-
 .loader-block:nth-child(2) {
   -moz-transform: translate(2.66667em, 0);
   -ms-transform: translate(2.66667em, 0);
@@ -95,7 +124,6 @@ export default {
   -webkit-animation-delay: 0.13s;
   animation-delay: 0.13s;
 }
-
 .loader-block:nth-child(3) {
   -moz-transform: translate(5.33333em, 0);
   -ms-transform: translate(5.33333em, 0);
@@ -104,7 +132,6 @@ export default {
   -webkit-animation-delay: 0.195s;
   animation-delay: 0.195s;
 }
-
 .loader-block:nth-child(4) {
   -moz-transform: translate(0, 2.66667em);
   -ms-transform: translate(0, 2.66667em);
@@ -113,7 +140,6 @@ export default {
   -webkit-animation-delay: 0.325s;
   animation-delay: 0.325s;
 }
-
 .loader-block:nth-child(5) {
   -moz-transform: translate(2.66667em, 2.66667em);
   -ms-transform: translate(2.66667em, 2.66667em);
@@ -122,7 +148,6 @@ export default {
   -webkit-animation-delay: 0.13s;
   animation-delay: 0.13s;
 }
-
 .loader-block:nth-child(6) {
   -moz-transform: translate(5.33333em, 2.66667em);
   -ms-transform: translate(5.33333em, 2.66667em);
@@ -131,7 +156,6 @@ export default {
   -webkit-animation-delay: 0.455s;
   animation-delay: 0.455s;
 }
-
 .loader-block:nth-child(7) {
   -moz-transform: translate(0, 5.33333em);
   -ms-transform: translate(0, 5.33333em);
@@ -140,7 +164,6 @@ export default {
   -webkit-animation-delay: 0.39s;
   animation-delay: 0.39s;
 }
-
 .loader-block:nth-child(8) {
   -moz-transform: translate(2.66667em, 5.33333em);
   -ms-transform: translate(2.66667em, 5.33333em);
@@ -149,7 +172,6 @@ export default {
   -webkit-animation-delay: 0.26s;
   animation-delay: 0.26s;
 }
-
 .loader-block:nth-child(9) {
   -moz-transform: translate(5.33333em, 5.33333em);
   -ms-transform: translate(5.33333em, 5.33333em);
@@ -180,7 +202,7 @@ export default {
     background: #2d2764;
   }
   to {
-    background: #dadada;
+    background: #dadada;  
   }
 }
 @keyframes show {
@@ -198,11 +220,10 @@ body {
 }
 
 body {
-  background: #999 url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4gPHN2ZyB2ZXJzaW9uPSIxLjEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGRlZnM+PHJhZGlhbEdyYWRpZW50IGlkPSJncmFkIiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSIgY3g9ImNpcmNsZSIgY3k9ImZhcnRoZXN0LXNpZGUiIHI9IjEwMCUiPjxzdG9wIG9mZnNldD0iMCUiIHN0b3AtY29sb3I9IiMzMzMzMzMiLz48c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiMxMTExMTEiLz48L3JhZGlhbEdyYWRpZW50PjwvZGVmcz48cmVjdCB4PSIwIiB5PSIwIiB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiBmaWxsPSJ1cmwoI2dyYWQpIiAvPjwvc3ZnPiA=');
+  background: #999 url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4gPHN2ZyB2ZXJzaW9uPSIxLjEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGRlZnM+PHJhZGlhbEdyYWRpZW50IGlkPSJncmFkIiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSIgY3g9ImNpcmNsZSIgY3k9ImZhcnRoZXN0LXNpZGUiIHI9IjEwMCUiPjxzdG9wIG9mZnNldD0iMCUiIHN0b3AtY29sb3I9IiMzMzMzMzMiLz48c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiMxMTExMTEiLz48L3JhZGlhbEdyYWRpZW50PjwvZGVmcz48cmVjdCB4PSIwIiB5PSIwIiB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiBmaWxsPSJ1cmwoI2dyYWQpIiAvPjwvc3ZnPiA=');  
   background: #999 -moz-radial-gradient(circle farthest-side, #333333, #111111);
   background: #999 -webkit-radial-gradient(circle farthest-side, #333333, #111111);
   background: #999 radial-gradient(circle farthest-side, #333333, #111111);
 }
-
 
 </style>
